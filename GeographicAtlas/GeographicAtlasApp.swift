0c4ef81e9c27ded1e970c37d753f2fa33b75dff3
zhaslan057 <z.tanerbergenov@gmail.com>
2023-05-16 22:52:44 +0600

    Initial Commit

---
+++ GeographicAtlas/GeographicAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeographicAtlasApp.swift
+//  GeographicAtlas
+//
+//  Created by Жаслан Танербергенов on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GeographicAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
